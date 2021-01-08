cf47e5052bdc841a67c894c9478d55aed837c575
DSJ-12 <77137369+DSJ-12@users.noreply.github.com>
2021-01-07 20:21:01 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+import math
+
+Temperature = 75
+Day = 1
+
+while Day < Temperature:
+    Temperature = Temperature - 1
+while Day == Temperature:
+    Temperature = random.uniform(60, 100)
+    Temperature = round(Temperature)
+    Temperature + 1
+while Day == 90:
+    Temperature = random.uniform(50, 70)
+
+while Day == 180:
+    Temperature = random.uniform(80, 110)
+
+while Day == 270:
+    Temperature = random.uniform(75, 90)
+while Day == 365:
+    Year = 1
+    Day = 0
+while 0 == 0:
+    Day = Day + 1
+    print(Day)
+    print(Temperature)
